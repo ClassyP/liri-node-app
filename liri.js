465161323db3d37eea392713d452d79d8a17bcc4
classyp <jeanphilip@me.com>
2019-05-21 21:07:01 -0700

    added new fuctions to liri, removed extra .env file

--- liri.js
+++ liri.js
@@ -2,29 +2,39 @@
 // =====================================
 // Read and set environment variables
 require("dotenv").config();
+
 // Import the Twitter NPM package.
 var Twitter = require("twitter");
+
 // Import the node-spotify-api NPM package.
 var Spotify = require("node-spotify-api");
+
 // Import the API keys
 var keys = require("./keys");
+
 // Import the request npm package.
 var request = require("request");
+
 // Import the FS package for read/write.
 var fs = require("fs");
+
 // Initialize the spotify API client using our client id and secret
 var spotify = new Spotify(keys.spotify);
+
 // FUNCTIONS
 // =====================================
+
 // Writes to the log.txt file
 var getArtistNames = function(artist) {
   return artist.name;
 };
+
 // Function for running a Spotify search
 var getMeSpotify = function(songName) {
   if (songName === undefined) {
     songName = "What's my age again";
   }
+
   spotify.search(
     {
       type: "track",
@@ -35,7 +45,9 @@ var getMeSpotify = function(songName) {
         console.log("Error occurred: " + err);
         return;
       }
+
       var songs = data.tracks.items;
+
       for (var i = 0; i < songs.length; i++) {
         console.log(i);
         console.log("artist(s): " + songs[i].artists.map(getArtistNames));
@@ -47,17 +59,15 @@ var getMeSpotify = function(songName) {
     }
   );
 };
+
 // Function for running a Twitter Search
 var getMyTweets = function() {
   var client = new Twitter(keys.twitter);
+
   var params = {
-    screen_name: "BizarroTest"
+    screen_name: "cnn"
   };
-  client.get("statuses/user_timeline", params, function(
-    error,
-    tweets,
-    response
-  ) {
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
     if (!error) {
       for (var i = 0; i < tweets.length; i++) {
         console.log(tweets[i].created_at);
@@ -67,18 +77,19 @@ var getMyTweets = function() {
     }
   });
 };
+
 // Function for running a Movie Search
 var getMeMovie = function(movieName) {
   if (movieName === undefined) {
     movieName = "Mr Nobody";
   }
-  var urlHit =
-    "http://www.omdbapi.com/?t=" +
-    movieName +
-    "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
+
   request(urlHit, function(error, response, body) {
     if (!error && response.statusCode === 200) {
       var jsonData = JSON.parse(body);
+
       console.log("Title: " + jsonData.Title);
       console.log("Year: " + jsonData.Year);
       console.log("Rated: " + jsonData.Rated);
@@ -91,18 +102,23 @@ var getMeMovie = function(movieName) {
     }
   });
 };
+
 // Function for running a command based on text file
 var doWhatItSays = function() {
   fs.readFile("random.txt", "utf8", function(error, data) {
     console.log(data);
+
     var dataArr = data.split(",");
+
     if (dataArr.length === 2) {
       pick(dataArr[0], dataArr[1]);
-    } else if (dataArr.length === 1) {
+    }
+    else if (dataArr.length === 1) {
       pick(dataArr[0]);
     }
   });
 };
+
 // Function for determining which command is executed
 var pick = function(caseData, functionData) {
   switch (caseData) {
@@ -122,10 +138,12 @@ var pick = function(caseData, functionData) {
       console.log("LIRI doesn't know that");
   }
 };
+
 // Function which takes in command line arguments and executes correct function accordigly
 var runThis = function(argOne, argTwo) {
   pick(argOne, argTwo);
 };
+
 // MAIN PROCESS
 // =====================================
 runThis(process.argv[2], process.argv[3]);
